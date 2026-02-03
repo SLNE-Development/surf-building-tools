a5a4b463ff9d09f508ddaf64319b7292f54d0ec7
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-03 13:04:35 +0100

    feat: add `teleportToHighestSpawn` utility and integrate into fall handling logic
    - Introduced `World.teleportToHighestSpawn` to teleport players to the highest safe spawn location.
    - Updated `PlayerMoveListener` to use the new utility for handling player falls below Y -70.

--- surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/listener/PlayerMoveListener.kt
+++ surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/listener/PlayerMoveListener.kt
@@ -1,6 +1,7 @@
 package dev.slne.surf.building.paper.listener
 
 import dev.slne.surf.building.paper.util.isBuildingWorld
+import dev.slne.surf.building.paper.util.teleportToHighestSpawn
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -14,7 +15,7 @@ object PlayerMoveListener : Listener {
         }
 
         if (event.to.y < -70) {
-            event.player.teleportAsync(event.to.world.spawnLocation).thenRun {
+            event.to.world.teleportToHighestSpawn(event.player).thenRun {
                 event.player.sendText {
                     appendSuccessPrefix()
                     success("Du wurdest zum Spawn teleportiert, da du aus der Welt gefallen bist.")
--- surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/util/building-world-util.kt
+++ surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/util/building-world-util.kt
@@ -3,6 +3,7 @@ package dev.slne.surf.building.paper.util
 import dev.slne.surf.building.paper.service.buildingWorldService
 import org.bukkit.World
 import org.bukkit.entity.Player
+import java.util.concurrent.CompletableFuture
 
 fun generateBuildingWorldId(): String {
     val charset = ('a'..'z') + ('0'..'9')
@@ -29,6 +30,20 @@ fun Player.currentBuildingWorld() =
     buildingWorldService.buildingWorlds.find { buildingWorld ->
         buildingWorld.world?.uid == this.world.uid
     }
+
+fun World.teleportToHighestSpawn(player: Player): CompletableFuture<Boolean> {
+    val spawnX = this.spawnLocation.x.toInt()
+    val spawnZ = this.spawnLocation.z.toInt()
+    val highestY = this.getHighestBlockYAt(spawnX, spawnZ)
+
+    return player.teleportAsync(
+        this.getBlockAt(spawnX, highestY, spawnZ).location.add(
+            0.5,
+            1.0,
+            0.5
+        )
+    )
+}
 
 
 
