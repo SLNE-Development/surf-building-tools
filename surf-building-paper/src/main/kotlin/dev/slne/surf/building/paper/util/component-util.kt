17e9e5d5049947eff3489e56a3ee82a7233af365
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-03 13:31:41 +0100

    feat: add `displayKey` utility for key-based MiniMessage deserialization
    - Introduced `SurfComponentBuilder.displayKey` for rendering key-based messages using `MiniMessage`.

--- surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/util/component-util.kt
+++ surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/util/component-util.kt
@@ -3,6 +3,7 @@ package dev.slne.surf.building.paper.util
 import dev.slne.surf.surfapi.core.api.messages.builder.SurfComponentBuilder
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.minimessage.MiniMessage
 
 fun SurfComponentBuilder.infoColored(text: Any, vararg decoration: TextDecoration) =
     coloredComponent(
@@ -21,3 +22,8 @@ fun SurfComponentBuilder.noteColored(text: Any, vararg decoration: TextDecoratio
         text.toString(),
         TextColor.color(27, 218, 224), *decoration
     )
+
+fun SurfComponentBuilder.displayKey(key: String) =
+    append(
+        MiniMessage.miniMessage().deserialize("<key:key.$key>")
+    ) // https://minecraft.fandom.com/wiki/Key_codes
