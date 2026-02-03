81809e110a474d60f1fabdc256152ccb4f37bebb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-03 13:56:35 +0100

    feat: restrict entity interaction in worlds with `allowBuild` disabled
    - Added `onInteractAtEntity` listener to cancel interactions with entities when building is disabled.
    - Informed players with an error message if interaction is blocked.

--- surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/listener/PlayerWorldStatusListener.kt
+++ surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/listener/PlayerWorldStatusListener.kt
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 
 object PlayerWorldStatusListener : Listener {
@@ -35,6 +36,18 @@ object PlayerWorldStatusListener : Listener {
 
     @EventHandler
     fun onInteract(event: PlayerInteractEvent) {
+        if (event.player.world.buildingWorld?.status?.allowBuild == false) {
+            event.isCancelled = true
+
+            event.player.sendText {
+                appendErrorPrefix()
+                error("Bauen ist in dieser Welt deaktiviert.")
+            }
+        }
+    }
+
+    @EventHandler
+    fun onInteractAtEntity(event: PlayerInteractAtEntityEvent) {
         if (event.player.world.buildingWorld?.status?.allowBuild == false) {
             event.isCancelled = true
 
