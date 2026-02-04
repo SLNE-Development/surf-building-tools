23b917619b10a958c39c3f931d00c9557d0ebad2
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-04 10:52:43 +0100

    refactor: streamline `BuildingWorldGenerator` and remove unused features
    - Removed redundant generation methods (e.g., caves, decorations, mobs).
    - Simplified chunk data initialization and added spawn location logic.

--- surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/world/generator/BuildingWorldGenerator.kt
+++ surf-building-paper/src/main/kotlin/dev/slne/surf/building/paper/world/generator/BuildingWorldGenerator.kt
@@ -1,6 +1,7 @@
 package dev.slne.surf.building.paper.world.generator
 
-import org.bukkit.Material
+import org.bukkit.Location
+import org.bukkit.World
 import org.bukkit.generator.ChunkGenerator
 import org.bukkit.generator.WorldInfo
 import java.util.*
@@ -13,66 +14,36 @@ object BuildingWorldGenerator : ChunkGenerator() {
         chunkZ: Int,
         chunkData: ChunkData
     ) {
-        for (x in 0..15) {
-            for (z in 0..15) {
-                for (y in worldInfo.minHeight..worldInfo.maxHeight) {
-                    chunkData.setBlock(x, y, z, Material.AIR)
-                }
-            }
-        }
     }
 
-    override fun shouldGenerateCaves() = false
-
-    override fun shouldGenerateDecorations() = false
-
-    override fun shouldGenerateMobs() = false
-
-    override fun shouldGenerateNoise() = false
-
-    override fun shouldGenerateStructures() = false
-
-    override fun shouldGenerateSurface() = false
-
-    override fun shouldGenerateCaves(
+    override fun generateNoise(
         worldInfo: WorldInfo,
         random: Random,
         chunkX: Int,
-        chunkZ: Int
-    ) = false
+        chunkZ: Int,
+        chunkData: ChunkData
+    ) {
+    }
 
-    override fun shouldGenerateDecorations(
+    override fun generateBedrock(
         worldInfo: WorldInfo,
         random: Random,
         chunkX: Int,
-        chunkZ: Int
-    ) = false
+        chunkZ: Int,
+        chunkData: ChunkData
+    ) {
+    }
 
-    override fun shouldGenerateMobs(
+    override fun generateCaves(
         worldInfo: WorldInfo,
         random: Random,
         chunkX: Int,
-        chunkZ: Int
-    ) = false
+        chunkZ: Int,
+        chunkData: ChunkData
+    ) {
+    }
 
-    override fun shouldGenerateNoise(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int
-    ) = false
-
-    override fun shouldGenerateStructures(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int
-    ) = false
-
-    override fun shouldGenerateSurface(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int
-    ) = false
+    override fun canSpawn(world: World, x: Int, z: Int) = true
+    override fun getFixedSpawnLocation(world: World, random: Random) =
+        Location(world, 0.5, 100.0, 0.5)
 }
