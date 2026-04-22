a17bd4cfa8d360f1ad4bec3e4743a706718aaab2
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-22 18:10:24 +0200

    refactor: remove unused chunk generation methods from BuildingWorldGenerator

--- src/main/kotlin/dev/slne/surf/building/world/generator/BuildingWorldGenerator.kt
+++ src/main/kotlin/dev/slne/surf/building/world/generator/BuildingWorldGenerator.kt
@@ -3,46 +3,9 @@ package dev.slne.surf.building.world.generator
 import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.generator.ChunkGenerator
-import org.bukkit.generator.WorldInfo
 import java.util.*
 
 object BuildingWorldGenerator : ChunkGenerator() {
-    override fun generateSurface(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int,
-        chunkData: ChunkData
-    ) {
-    }
-
-    override fun generateNoise(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int,
-        chunkData: ChunkData
-    ) {
-    }
-
-    override fun generateBedrock(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int,
-        chunkData: ChunkData
-    ) {
-    }
-
-    override fun generateCaves(
-        worldInfo: WorldInfo,
-        random: Random,
-        chunkX: Int,
-        chunkZ: Int,
-        chunkData: ChunkData
-    ) {
-    }
-
     override fun canSpawn(world: World, x: Int, z: Int) = true
     override fun getFixedSpawnLocation(world: World, random: Random) =
         Location(world, 0.5, 100.0, 0.5)
